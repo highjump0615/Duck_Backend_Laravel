27cf77052e16dfdd0cc6bb08ff548c607d52956b
highjump0615 <lixiang901112@163.com>
2017-07-12 18:20:59 +0800

    Added login module

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -36,4 +37,15 @@ class LoginController extends Controller
     {
         $this->middleware('guest', ['except' => 'logout']);
     }
+
+//    /**
+//     * 登录成功
+//     * @param Request $request
+//     * @param $user
+//     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+//     */
+//    protected function authenticated(Request $request, $user)
+//    {
+//        return redirect('/');
+//    }
 }
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>mini shop</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?=asset('css/login.css') ?>" rel="stylesheet">
+</head>
+<body>
+    <div class="login-page">
+        <div class="text-center">
+            <h3 style="color: white;"><strong>皇老鸭</strong></h3>
+        </div>
+        <div class="form">
+            <form action="/login" method="POST">
+                {{ csrf_field() }}
+                <input type="text" placeholder="用户名" name="username">
+                <input type="password" placeholder="密码" name="password">
+                <button type="submit">登录</button>
+                <p class="message">忘了密码？ <a href="/login/forgot">找回密码</a></p>
+            </form>
+        </div>
+    </div>
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Bootstrap Javascript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Custom Javascript -->
+    <script src="<?=asset('js/login.js') ?>"></script>
+</body>
+</html>
